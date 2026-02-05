9eed162a402004898ea129778366c0c27c16c929
unknown <magnus.taarnberg@gmail.com>
2026-02-05 14:35:25 +0100

    git commit

--- simple_classifier.py
+++ simple_classifier.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
 from matplotlib.colors import ListedColormap
 from sklearn import datasets, neighbors
 from sklearn.inspection import DecisionBoundaryDisplay
